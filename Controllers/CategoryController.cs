000325e67fb107317d120fc68dcbc68a945b01ca
Lucas Oliveira Lopes <lucas.oliveira.lopes2254@gmail.com>
2023-06-14 21:46:58 -0300

    Add project files.

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,118 @@
+﻿using Blog.Data;
+using Blog.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Controllers {
+    [ApiController]
+    public class CategoryController : ControllerBase {
+        [HttpGet("v1/categories")]
+        public async Task<IActionResult> GetAsync(
+            [FromServices] BlogDataContext context)
+        {
+			try {
+				var categories = await context.Categories.ToListAsync();
+				return Ok(categories);
+			}
+
+			catch (Exception ex) {
+				return StatusCode(500, "05X05 - Falha interna no servidor");
+			}
+		}
+
+		[HttpGet("v1/categories/{id:int}")]
+		public async Task<IActionResult> GetByIdAsync(
+            [FromRoute] int id,
+			[FromServices] BlogDataContext context) {
+			try {
+				var category = await context.Categories.FirstOrDefaultAsync(c => c.Id == id);
+
+				if (category == null)
+					return NotFound();
+
+				return Ok(category);
+			}
+
+			catch (Exception ex) {
+				return StatusCode(500, "05X06 - Falha interna no servidor");
+			}
+		}
+
+		[HttpPost("v1/categories")]
+		public async Task<IActionResult> PostAsync(
+			[FromBody] Category model,
+			[FromServices] BlogDataContext context)
+		{
+			try {
+				await context.Categories.AddAsync(model);
+				await context.SaveChangesAsync();
+
+				return Created($"v1/categories/{model.Id}", model);
+			}
+
+			catch (DbUpdateException ex) {
+				return StatusCode(500, "05XE9 - Não foi possível incluir a categoria");
+			}
+
+			catch (Exception ex) {
+				return StatusCode(500, "05X10 - Falha interna no servidor");
+			}
+
+		}
+
+		[HttpPut("v1/categories/{id:int}")]
+		public async Task<IActionResult> PutAsync(
+			[FromRoute] int id,
+			[FromBody] Category model,
+			[FromServices] BlogDataContext context)
+		{
+			try {
+				var category = await context.Categories.FirstOrDefaultAsync(c => c.Id == id);
+
+				if (category == null)
+					return NotFound();
+
+				category.Name = model.Name;
+				category.Slug = model.Slug;
+
+				context.Categories.Update(category);
+				await context.SaveChangesAsync();
+
+				return Ok(category);
+			}
+
+			catch (DbUpdateException ex) {
+				return StatusCode(500, "05XE8 - Não foi possível alterar a categoria");
+			}
+
+			catch (Exception ex) {
+				return StatusCode(500, "05X11 - Falha interna no servidor");
+			}
+		}
+
+		[HttpDelete("v1/categories/{id:int}")]
+		public async Task<IActionResult> DeleteAsync(
+			[FromRoute] int id,
+			[FromServices] BlogDataContext context) {
+			try {
+				var category = await context.Categories.FirstOrDefaultAsync(c => c.Id == id);
+
+				if (category == null)
+					return NotFound();
+
+				context.Categories.Remove(category);
+				await context.SaveChangesAsync();
+
+				return Ok(category);
+			}
+
+			catch (DbUpdateException ex) {
+				return StatusCode(500, "05XE7 - Não foi possível deletar a categoria");
+			}
+
+			catch (Exception ex) {
+				return StatusCode(500, "05X12 - Falha interna no servidor");
+			}
+		}
+	}
+}
---
+++ Data/BlogDataContext.cs
@@ -0,0 +1,25 @@
+using System;
+using Blog.Data.Mappings;
+using Blog.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Data{
+    public class BlogDataContext : DbContext{
+
+        public DbSet<Category> Categories { get; set;}
+        public DbSet<Post> Posts { get; set;}
+        public DbSet<User> Users { get; set;}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options){
+            options.UseSqlServer("");
+            //options.LogTo(Console.WriteLine);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new CategoryMap());
+            modelBuilder.ApplyConfiguration(new UserMap());
+            modelBuilder.ApplyConfiguration(new PostMap());
+        }
+    }
+}
